fcb6a96076f3a0e7d630f6321d59465f240ec41d
ivandrdr <117579315+ivandrdr@users.noreply.github.com>
2023-02-01 23:34:16 +0300

    Add skills, joysticks for mobile control

--- Tank Survivors Prototype/Assets/Scripts/System/DamageTextController.cs
+++ Tank Survivors Prototype/Assets/Scripts/System/DamageTextController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using TMPro.EditorUtilities;
 using UnityEngine.Pool;
 
 public class DamageTextController : MonoBehaviour
